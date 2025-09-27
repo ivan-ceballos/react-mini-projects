d84d41bc63347dd161af368c4895b268e6a4df84
Iván Ceballos <ivan_ceballos2@hotmail.com>
2025-09-26 21:09:40 -0300

    extract constants and add winning combos

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -2,25 +2,22 @@ import { useState } from 'react'
 import './App.css'
 
 import { Square } from './components/Square'
-
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
+import { TURNS, WINNING_COMBOS } from './utils/constants'
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
   
-
-
   const updateBoard = (index) => {
+    // No actualizar si ya hay algo en la posición
     if (board[index]) return
 
+    // Actualizar el tablero
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
 
+    // Cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
   }
