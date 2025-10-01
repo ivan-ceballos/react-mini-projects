37207c734994af8cdba7dc9bddbd51126d574d58
Iván Ceballos <ivan_ceballos2@hotmail.com>
2025-10-01 17:39:41 -0300

    add cursor toggle effect

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from 'react'
+import './App.css'
 
 function App () {
   const [enable, setEnable] = useState(false)
+  const centerX = window.innerWidth / 2
+  const centerY = window.innerHeight / 2
+  const [position, setPosition] = useState({ x: centerX, y: centerY })
 
   useEffect(() => {
     const handleMove = (event) => {
-      const evento = event
-      console.log(evento)
+      const newEvent = event
+      setPosition({ x: newEvent.clientX, y: newEvent.clientY })
     }
 
     if (enable) {
@@ -20,10 +24,27 @@ function App () {
     }
   }, [enable])
 
+  useEffect(() => {
+    if (enable) {
+      document.body.classList.add('no-cursor')
+    } else {
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enable])
+
   return (
-    <button onClick={() => setEnable(!enable)}>
-      {enable ? 'Disable' : 'Enable'} Mouse Follow
-    </button>
+    <main>
+      <div
+        className='follower'
+        style={{ left: position.x, top: position.y }}
+      />
+      <button
+        onClick={() => setEnable(!enable)}
+        className='toggle-button'
+      >
+        {enable ? 'Disable' : 'Enable'} Mouse Follow
+      </button>
+    </main>
   )
 }
 
