8792b2ea09c15e28f533a8faa664c38470ee65d7
Iván Ceballos <ivan_ceballos2@hotmail.com>
2025-10-01 12:53:47 -0300

    add pointermove event listener

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from 'react'
+
 function App () {
+  const [enable, setEnable] = useState(false)
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const evento = event
+      console.log(evento)
+    }
+
+    if (enable) {
+      window.addEventListener('pointermove', handleMove)
+      console.log('effect', enable)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+      console.log('cleanup', enable)
+    }
+  }, [enable])
+
   return (
-    <h1>Hello world</h1>
+    <button onClick={() => setEnable(!enable)}>
+      {enable ? 'Disable' : 'Enable'} Mouse Follow
+    </button>
   )
 }
 
